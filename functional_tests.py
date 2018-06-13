59e559737ba50e26ae741b555a9ef03ffd374457
Vance <vmorris@us.ibm.com>
2018-06-13 16:59:02 -0500

    Can handle 1 list item, but no more. Potential refactoring required in functional test

--- functional_tests.py
+++ functional_tests.py
@@ -33,27 +33,33 @@ class NewVisitorTest(unittest.TestCase):
         # He types "Leard TDD" into a text box
         inputbox.send_keys('Learn TDD')
 
-        # Whe he hits enter, the page updates, and now the page lists
+        # When he hits enter, the page updates, and now the page lists
         # "1: Learn TDD" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Learn TDD' for row in rows),
-            "New to-do item did not appear in the table"
-        )
+        self.assertIn('1: Learn TDD', [row.text for row in rows])
 
         # There is still a text box inviting him to add another item.
         # He enters "Use TDD to build killer web app"
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use TDD to build a killer web app')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(3)
 
         # The page updates again, and now both items show on his list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Learn TDD', [row.text for row in rows])
+        self.assertIn('2: Use TDD to build a killer web app', [row.text for row in rows])
+
 
         # Vance wonders whether the site will remember his list. The site
         # generated a unique URL for him and there is some explanatory text
         # to that effect.
+        self.fail('Finish the test!')
 
         # He visits that URL - his to-do list is still there.
 
