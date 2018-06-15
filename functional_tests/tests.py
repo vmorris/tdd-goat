28faf298b7dd61e7ef07fd330659cddd4e90cff8
Vance <vmorris@us.ibm.com>
2018-06-15 15:44:07 -0500

    get rid of voodoo sleeps

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,7 +1,10 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
+
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -11,10 +14,18 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
 
@@ -40,19 +51,17 @@ class NewVisitorTest(LiveServerTestCase):
         # When he hits enter, the page updates, and now the page lists
         # "1: Learn TDD" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1: Learn TDD')
+        self.wait_for_row_in_list_table('1: Learn TDD')
 
         # There is still a text box inviting him to add another item.
         # He enters "Use TDD to build killer web app"
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use TDD to build a killer web app')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # The page updates again, and now both items show on his list
-        self.check_for_row_in_list_table('1: Learn TDD')
-        self.check_for_row_in_list_table('2: Use TDD to build a killer web app')
+        self.wait_for_row_in_list_table('1: Learn TDD')
+        self.wait_for_row_in_list_table('2: Use TDD to build a killer web app')
 
         # Vance wonders whether the site will remember his list. The site
         # generated a unique URL for him and there is some explanatory text
